3e72789e67c69d89b2c7c58f0e06f8f6d1489dce
Cathut <necota2496@gmail.com>
2024-03-02 23:19:18 +0900

    スクリプタブルオブジェクト作成の仮関数（メニュー）追加

--- Assets/CatHut/Editor/MasterDataEditor/EditorWindow/MasterDataEditorSettingsWindow.cs
+++ Assets/CatHut/Editor/MasterDataEditor/EditorWindow/MasterDataEditorSettingsWindow.cs
@@ -14,26 +14,39 @@ public class MasterDataEditorSettingsWindow : EditorWindow
     private void OnEnable()
     {
         // 初期化コード
+        //設定ファイルをロード
         MasterDataEditorConfig.LoadSettings();
         csvMasterDataFolder = MasterDataEditorConfig.settings.CsvMasterDataPath;
     }    
 
     private void OnGUI()
     {
+        //表題
+        GUILayout.Space(20);
         GUILayout.Label("MasterDataEditor Settings", EditorStyles.boldLabel);
 
+        //指定されたフォルダがない場合に警告を表示
+        GUILayout.Space(5);
         if (!AssetDatabase.IsValidFolder(csvMasterDataFolder))
         {
             GUIStyle errorStyle = new GUIStyle();
             errorStyle.normal.textColor = Color.red;
             GUILayout.Label("指定されたフォルダが存在しません。", errorStyle);
         }
+
+        //設定項目
         csvMasterDataFolder = EditorGUILayout.TextField("CsvMasterDataPath", csvMasterDataFolder);
 
-        if (GUILayout.Button("Save Settings"))
+        // 保存ボタン
+        GUILayout.Space(10);
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save Settings", GUILayout.Width(200)))
         {
             MasterDataEditorConfig.settings.CsvMasterDataPath = csvMasterDataFolder;
             MasterDataEditorConfig.SaveSettings();
         }
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
     }
 }
---
+++ Assets/CatHut/Editor/MasterDataEditor/Menu/MasterDataEditorMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEditor;
+
+public static class MasterDataEditorMenu
+{
+    // メニューバーの 'Tools > CatHut > MasterDataEditor > Create ScriptableObject' をクリックした時に実行されるメソッド
+    [MenuItem("Tools/CatHut/MasterDataEditor/Create ScriptableObject")]
+    private static void CreateScriptableObject()
+    {
+        // ここにScriptableObjectの作成ロジックを実装します。
+        // 現時点では空関数としています。
+    }
+}
