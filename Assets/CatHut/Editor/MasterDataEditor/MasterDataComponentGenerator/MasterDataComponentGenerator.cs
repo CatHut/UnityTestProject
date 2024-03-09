fd2b7facfab6a0b62a211bca188bce8e6588086e
Cathut <necota2496@gmail.com>
2024-03-09 17:38:30 +0900

    一旦エラーのない状態に SingletonMonoBehaviour Addressable追加

---
+++ Assets/CatHut/Editor/MasterDataEditor/MasterDataComponentGenerator/MasterDataComponentGenerator.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using static MasterDataEditorConfig;
+
+namespace CatHut
+{
+    public static class MasterDataComponentGenerator
+    {
+        private static void CreateMasterDataClass(Dictionary<string, ExcelDataDictionary> ExcelDataDic)
+        {
+            //MasterDataのメンバ宣言文字列を作成
+            var MasterDataClassDeclareStr = GetMasterDataClassDeclareStr(ExcelDataDic);
+
+            //MasterDataのAssetロード処理の文字列作成
+            var MasterDataClassLoadStr = GetMasterDataClassLosdStr(ExcelDataDic);
+
+            //テンプレートファイルを探す
+            var TemplateFileGUIDs = AssetDatabase.FindAssets(UsingExcelCommon.MasterDataTemplate);
+            var TemplateFile = "";
+
+            TemplateFile = AssetDatabase.GUIDToAssetPath(TemplateFileGUIDs[0]);
+
+            var FileStr = File.ReadAllText(TemplateFile);
+
+
+            FileStr = FileStr.Replace("#MasterDataClassDeclare#", MasterDataClassDeclareStr);
+            FileStr = FileStr.Replace("#MasterDataClassLoad#", MasterDataClassLoadStr);
+
+            TemplateFile = TemplateFile.Replace("/Template", "/CreatedScriptableObject");
+            File.WriteAllText(TemplateFile.Replace(UsingExcelCommon.MasterDataTemplate + ".txt", "MasterData.cs"), FileStr, Encoding.UTF8);
+
+        }
+
+    }
+
+}
+#endif
---
+++ Assets/CatHut/MasterData/MasterData.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+
+
+public class MasterData : SingletonMonoBehaviour<MasterData>
+{
+
+    //Created MasterData
+    private Enemy _EnemyData;
+    public Enemy EnemyData
+    {
+        get { return _EnemyData; }
+        set { _EnemyData = value; } 
+    }
+
+
+    private new void Awake()
+    {
+        base.Awake();
+
+        EnemyData = Addressables.LoadAssetAsync<Enemy>("Enemy").WaitForCompletion();
+
+    }
+
+    public object this[string propertyName]
+    {
+        get
+        {
+            return typeof(MasterData).GetProperty(propertyName).GetValue(this);
+        }
+
+        set
+        {
+            typeof(MasterData).GetProperty(propertyName).SetValue(this, value);
+        }
+    }
+
+    public List<string> PropertyNames
+    {
+        get
+        {
+            var ret = new List<string>();
+            var properties = this.GetType().GetProperties()
+                .Where(p => p.PropertyType != typeof(System.Object) && p.Name != "PropertyNames" && p.DeclaringType == this.GetType())
+                .ToArray();
+            // インデクサによるItemプロパティ(System.Ojbect)を除外
+            // 基底クラスのプロパティを除外
+
+            foreach (var property in properties)
+            {
+                ret.Add(property.Name);
+            }
+
+            return ret;
+        }
+    }
+}
+
